159307f39e4845d6851b0949ccb4dc2ab1fc7a2d
AsierConesa <asierconesa2002@gmail.com>
2021-11-10 11:51:32 +0100

    creada interfaz y avanzado en clases Shield y Cell

--- BridgeCrewAsier/BridgeCrewAsier/Cell.cs
+++ BridgeCrewAsier/BridgeCrewAsier/Cell.cs
@@ -8,5 +8,20 @@ namespace BridgeCrewAsier
 {
     class Cell
     {
+        public int x { get; set; }
+        public int y { get; set; }
+        Boolean broken { get; set; }
+
+        public Cell(int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.broken = false;
+        }
+        public Boolean equals(Cell c2) {
+            if ((this.x == c2.x) && (this.y == c2.y))
+                return true;
+            else
+                return false;
+        }
     }
 }
--- BridgeCrewAsier/BridgeCrewAsier/Shield.cs
+++ BridgeCrewAsier/BridgeCrewAsier/Shield.cs
@@ -8,5 +8,23 @@ namespace BridgeCrewAsier
 {
     class Shield
     {
+        Cell[,] shield;
+
+        public Shield(int row, int col)
+        {
+            shield = new Cell[row, col];
+            for (int i = 1; i <= row * col; i++)
+            {
+                int x = row % i;
+                int y = (int)i / row;
+                shield[x, y] = new Cell(x, y);
+            }
+        }
+
+        public void fixPath(Cell source, Cell target)
+        {
+            //con calma me lo implementas bro
+
+        }
     }
 }
