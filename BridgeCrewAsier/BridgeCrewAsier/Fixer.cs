1138a718a01c36425272abfd0796334abddc48ff
AsierConesa <asierconesa2002@gmail.com>
2021-11-14 14:58:20 +0100

    mejorada eficiencia de la reparación

--- BridgeCrewAsier/BridgeCrewAsier/Fixer.cs
+++ BridgeCrewAsier/BridgeCrewAsier/Fixer.cs
@@ -16,9 +16,22 @@ namespace BridgeCrewAsier
             while (!(source.equals(target)))
             {
                 Random randomizator = new Random();
-                int i = randomizator.Next(-1, 2); //coger numero aleatorio del -1 al 1 (i)
+                int i;
+                int j;
 
-                int j = randomizator.Next(-1, 2); //coger numero aleatorio del -1 al 1 (j)
+                if (source.getRow() == target.getRow())
+                    i = 0;
+                else if(source.getRow() > target.getRow())
+                    i = randomizator.Next(-1, 1); //coger numero aleatorio del -1 al 0 (i)
+                else
+                    i = randomizator.Next(0, 2); //coger numero aleatorio del 0 al 1 (i)
+
+                if (source.getColumn() == target.getColumn())
+                    j = 0;
+                else if(source.getColumn() > target.getColumn())
+                    j = randomizator.Next(-1, 1); //coger numero aleatorio del -1 al 0 (j)
+                else
+                    j = randomizator.Next(0, 2); //coger numero aleatorio del 0 al 1 (j)
 
                 aux = new Cell(source.getRow() + i, source.getColumn() + j);
                 if (board.isInside(aux))
